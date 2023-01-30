562a4927584b7dbdfb0c52b93b389acd2b0a7dbc
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-01-30 16:49:25 -0300

    Adicionando mais itens ao catálogo e estruturando melhor

--- src/components/DataRequest/index.jsx
+++ src/components/DataRequest/index.jsx
@@ -3,7 +3,7 @@ import './DataRequest.css'
 import api from 'services/api'
 
 import { useEffect, useState } from 'react';
-import BannerPresentation from 'components/BannerPresentation';
+import { AiFillStar, AiTwotoneCalendar} from 'react-icons/ai';
 
 const DataRequest = ({url, title}) => {
   const [movies, setMovies] = useState([]);
@@ -14,12 +14,12 @@ const DataRequest = ({url, title}) => {
       const response = await api.get(`${url}`, {
         params:{
           api_key: "3d2aef7e99d7de4c813670a2c4201b76",
-          language: "pt-BR",
+          language: "en-US",
           page: 1,
         }
       })
       
-      setMovies(response.data.results.slice(0, 7));
+      setMovies(response.data.results.slice(0, 10));
     }
 
     loadMovies();
@@ -29,14 +29,19 @@ const DataRequest = ({url, title}) => {
   return (
     
     <div className='movies'>
-      <BannerPresentation />
       <h3>{title}</h3>
       <div className='presentation-list'>
         {movies.length > 0 && movies.map((movie) => {
           return(
-            <article key={movie.id} 
-              style={{backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.poster_path}")`}}>
-              <strong>{movie.title}</strong>
+            <article 
+              key={movie.id} 
+              style={{backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.poster_path}")`}}
+            > 
+              <div className='info-basic'>
+                <p>
+                  {movie.title} <br/> <AiFillStar></AiFillStar> {movie.vote_average} | <AiTwotoneCalendar></AiTwotoneCalendar> {movie.release_date} 
+                </p>
+              </div>
             </article>
           )
    
--- src/pages/Catalog/index.jsx
+++ src/pages/Catalog/index.jsx
@@ -2,12 +2,16 @@ import './Catalog.css'
 
 import DataRequest from 'components/DataRequest';
 import HeaderCatalog from 'components/HeaderCatalog';
+import BannerPresentation from 'components/BannerPresentation';
 
 const Catalog = () => {
   return (
     <div className='catalog'>
       <HeaderCatalog />
+      <BannerPresentation />
       <DataRequest url={"movie/now_playing"} title={"In Theaters:"}/>
+      <DataRequest url={"movie/top_rated"} title={"Top Rated:"}/>
+      <DataRequest url={"movie/popular"} title={"Populars:"}/>
     </div>
   )
 }
