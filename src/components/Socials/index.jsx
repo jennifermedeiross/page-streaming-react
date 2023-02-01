8305850177fb3489ba926f419ee21d6fae5597ae
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-02-01 20:13:19 -0300

    Adicionando links de contato

--- src/components/Socials/index.jsx
+++ src/components/Socials/index.jsx
@@ -4,19 +4,15 @@ import { BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs';
 
 const Socials = () => {
 
-  function handleSocial () {
-    return window.open('https://github.com/jennifermedeiross/').focus
-  }
-
   return (
     <div className='social'>
       <div>
         <h2>Entre em contato!</h2>
       </div>
       <div className='icons-socials'>
-        <BsInstagram onClick={handleSocial} className='icon-social'></BsInstagram>
-        <BsGithub onClick={handleSocial} className='icon-social'></BsGithub>
-        <BsLinkedin onClick={handleSocial} className='icon-social'></BsLinkedin>
+        <BsInstagram onClick={() => window.open('https://www.instagram.com/jennifermedeiross/').focus} className='icon-social'></BsInstagram>
+        <BsGithub onClick={() => window.open('https://github.com/jennifermedeiross').focus} className='icon-social'></BsGithub>
+        <BsLinkedin onClick={() => window.open('https://www.linkedin.com/in/jennifermedeiross/').focus} className='icon-social'></BsLinkedin>
       </div>
     </div>
   )
